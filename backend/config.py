e481b72376a33e7c7898362419805178c84ba034
Muhammad Abdullah <muhammad0abdullah68@gamil.com>
2026-08-24 12:09:02 +0500

    settuped config.py with cloudinary url

---
+++ backend/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+    )
+    
+    CLOUDINARY_URL: SecretStr    
+    
+settings = Settings() #type: ignore
